a5c723454b266d6ca2c8646565e54e50f156e4d3
Aju Abraham <ajubabraham23@gmail.com>
2025-05-13 23:49:20 +0530

    seperated compilation and execution controller

--- server/src/routes/execution.routes.js
+++ server/src/routes/execution.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { executeCode } from "../controllers/execution.controller.js";
+import {
+  compileCode,
+  executeCode,
+} from "../controllers/execution.controller.js";
 
 const executionRouter = Router();
 
+executionRouter.post("/compile", authMiddleware, compileCode);
 executionRouter.post("/", authMiddleware, executeCode);
 
 export default executionRouter;
