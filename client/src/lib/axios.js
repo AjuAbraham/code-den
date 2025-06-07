78827ead6e4f00a7b66682d912c938c7f827eaff
Aju Abraham <ajubabraham23@gmail.com>
2025-06-07 21:06:57 +0530

    fixed axios url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_MODE,
+  baseURL: `${import.meta.env.VITE_MODE}api/v1`,
   withCredentials: true,
 });
 export const signUpUser = async (formData) => {
