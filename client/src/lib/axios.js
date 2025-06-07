14172ff4cb56de8a9077a1c0685dc5627a1bae96
Aju Abraham <ajubabraham23@gmail.com>
2025-06-07 19:06:46 +0530

    fix env in frontend

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_MODE,
   withCredentials: true,
 });
 export const signUpUser = async (formData) => {
