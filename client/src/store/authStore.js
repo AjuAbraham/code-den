0977cd510a07bdaf60f8397de126d3b08dbd2406
Aju Abraham <ajubabraham23@gmail.com>
2025-06-08 09:23:11 +0530

    fixed store

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,16 +1,10 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-
 const authStore = create(
-  persist(
-    (set) => ({
-      authUser: null,
-      setUser: (user) => set({ authUser: user }),
-    }),
-    {
-      name: "auth-storage",
-    }
-  )
+  persist((set) => ({
+    authUser: null,
+    setUser: (user) => set({ authUser: user }),
+  }))
 );
 
 export default authStore;
